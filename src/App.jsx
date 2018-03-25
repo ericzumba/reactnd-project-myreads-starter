05014fe0d102500a30253e739a27b9cfd2923f19
Eric Torti <ericzumba@gmail.com>
2018-03-25 13:16:37 -0300

    extracting SearchBooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
+import SearchBooks from './SearchBooks'
 import './App.css'
 
 class ShelfChanger extends React.Component {
@@ -58,45 +59,11 @@ class BookShelf extends React.Component {
   }
 }
 
-class SearchBooks extends React.Component {
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to="/">Close</Link>
-          <div className="search-books-input-wrapper">
-            {/*
-             NOTES: The search from BooksAPI is limited to a particular set of search terms.
-             You can find these search terms here:
-             https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-             you don't find a specific author or title. Every search is limited by search terms.
-             */}
-            <input type="text" placeholder="Search by title or author"/>
-
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
-      </div>
-    )
-  }
-}
-
 class BooksApp extends React.Component {
   constructor(props) {
     super(props)
     this.moveShelf = this.moveShelf.bind(this)
     this.state = {
-      /**
-       * TODO: Instead of using this state variable to keep track of which page
-       * we're on, use the URL in the browser's address bar. This will ensure that
-       * users can use the browser's back and forward buttons to navigate between
-       * pages, as well as provide a good URL they can bookmark and share.
-       */
-      showSearchPage: false,
       dictionary: {
         currentlyReading: "Currently Reading",
         wantToRead: "Want to Read",
@@ -211,7 +178,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )}/>
-        <Route path="/search" component= {SearchBooks}/>
+        <Route path="/search" component={SearchBooks}/>
       </div>
     )
   }
