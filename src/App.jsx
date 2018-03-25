36af64bd9107b6652ed1a4d4454ac74ba1281f69
Eric Torti <ericzumba@gmail.com>
2018-03-25 01:15:02 -0300

    selected options

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class ShelfChanger extends React.Component {
   render() {
     return (
       <div className="book-shelf-changer">
-        <select>
+        <select value={this.props.shelf}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
@@ -26,11 +26,13 @@ class Book extends React.Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover"
-                 style={{ width: this.props.width,
-                          height: this.props.height,
-                          backgroundImage: backgroundUrl }}>
+                 style={{
+                   width: this.props.width,
+                   height: this.props.height,
+                   backgroundImage: backgroundUrl
+                 }}>
             </div>
-            <ShelfChanger/>
+            <ShelfChanger book={{ title: this.props.title }} shelf={ this.props.shelf }/>
           </div>
           <div className="book-title">{ this.props.title }</div>
           <div className="book-authors">{ this.props.authors }</div>
@@ -60,7 +62,7 @@ class SearchBooks extends React.Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
           <div className="search-books-input-wrapper">
             {/*
              NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -91,71 +93,91 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
-    wantToRead: [],
-
+    currentlyReading: [
+      {
+        width: 128,
+        height: 193,
+        title: "To Kill a Mockingbird",
+        authors: "Harper Lee",
+        backgroundImage: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+      },
+      {
+        width: 128,
+        height: 193,
+        title: "Ender's Game",
+        authors: "Orson Scott Card",
+        backgroundImage: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+      }
+    ],
+    wantToRead: [
+      {
+        width: 128,
+        height: 193,
+        title: "1776",
+        authors: "David McCullough",
+        backgroundImage: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
+      },
+      {
+        width: 128,
+        height: 192,
+        title: "Harry Potter and the Sorcerer's Stone",
+        authors: "J.K. Rowling",
+        backgroundImage: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
+      }
+    ],
+    read: [
+      {
+        width: 128,
+        height: 192,
+        title: "The Hobbit",
+        authors: "J.R.R. Tolkien",
+        backgroundImage: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
+      },
+      {
+        width: 128,
+        height: 174,
+        title: "Oh, the Places You'll Go!",
+        authors: "Seuss",
+        backgroundImage: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
+      },
+      {
+        width: 128,
+        height: 192,
+        title: "The Adventures of Tom Sawyer",
+        authors: "Mark Twain",
+        backgroundImage: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
+      }
+    ]
   }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <SearchBooks/>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf title="Currently Reading">
-                  <Book width="128"
-                        height="193"
-                        title="To Kill a Mockingbird"
-                        authors="Harper Lee"
-                        backgroundImage="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"/>
-                  <Book width="128"
-                        height="188"
-                        title="Ender's Game"
-                        authors="Orson Scott Card"
-                        backgroundImage="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"/>
-                </BookShelf>
-                <BookShelf title="Want to Read">
-                  <Book width="128"
-                        height="193"
-                        title="1776"
-                        authors="David McCullough"
-                        backgroundImage="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"/>
-                  <Book width="128"
-                        height="192"
-                        title="Harry Potter and the Sorcerer's Stone"
-                        authors="J.K. Rowling"
-                        backgroundImage="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"/>
-                </BookShelf>
-                <BookShelf title="Read">
-                  <Book width="128"
-                        height="192"
-                        title="The Hobbit"
-                        authors="J.R.R. Tolkien"
-                        backgroundImage="http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"/>
-                  <Book width="128"
-                        height="174"
-                        title="Oh, the Places You'll Go!"
-                        authors="Seuss"
-                        backgroundImage="http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"/>
-                  <Book width="128"
-                        height="192"
-                        title="The Adventures of Tom Sawyer"
-                        authors="Mark Twain"
-                        backgroundImage="http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"/>
-
-                </BookShelf>
+            <SearchBooks/>
+          ) : (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <BookShelf title="Currently Reading">
+                    {this.state.currentlyReading.map(b => <Book {...b} shelf="currentlyReading"/>)}
+                  </BookShelf>
+                  <BookShelf title="Want to Read">
+                    {this.state.wantToRead.map(b => <Book {...b} shelf="wantToRead"/>)}
+                  </BookShelf>
+                  <BookShelf title="Read">
+                    {this.state.read.map(b => <Book {...b} shelf="read"/>)}
+                  </BookShelf>
+                </div>
+              </div>
+              <div className="open-search">
+                <a onClick={() => this.setState({showSearchPage: true})}>Add a book</a>
               </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+          )}
       </div>
     )
   }
