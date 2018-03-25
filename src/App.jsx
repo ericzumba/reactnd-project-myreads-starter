36ca490792ae2883d7040cf8c214b802a8849783
Eric Torti <ericzumba@gmail.com>
2018-03-25 00:25:56 -0300

    fix titles

--- src/App.jsx
+++ src/App.jsx
@@ -108,17 +108,25 @@ class BooksApp extends React.Component {
                 <BookShelf title="Currently Reading">
                   <Book width="128"
                         height="193"
+                        title="To Kill a Mockingbird"
+                        authors="Harper Lee"
                         backgroundImage="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"/>
                   <Book width="128"
                         height="188"
+                        title="Ender's Game"
+                        authors="Orson Scott Card"
                         backgroundImage="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"/>
                 </BookShelf>
                 <BookShelf title="Want to Read">
                   <Book width="128"
                         height="193"
+                        title="1776"
+                        authors="David McCullough"
                         backgroundImage="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"/>
                   <Book width="128"
                         height="192"
+                        title="Harry Potter and the Sorcerer's Stone"
+                        authors="J.K. Rowling"
                         backgroundImage="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"/>
                 </BookShelf>
                 <div className="bookshelf">
